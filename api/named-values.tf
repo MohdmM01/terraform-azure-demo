65974b03a9a2d53c08acd5a15f04f33fb5f59dcd
Mohd Anis Mohd Mokhtar <mohdanis.mohdmokhtar@mattheyasia.com>
2021-06-16 12:44:38 +0800

    Update named-values.tf
    1. Introduced new named values in APIM.

--- api/named-values.tf
+++ api/named-values.tf
@@ -6,3 +6,12 @@ resource "azurerm_api_management_named_value" "basic-auth-password" {
   value               = var.basic_auth_password
   secret              = true
 }
+
+resource "azurerm_api_management_named_value" "test-named-value" {
+  name                = "basic-auth-password"
+  resource_group_name = local.shared-rg 
+  api_management_name = local.apim_name
+  display_name        = "test-named-value"
+  value               = "test-named-value-value"
+  secret              = false
+}
