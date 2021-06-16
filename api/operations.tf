3d3bd557562273ec3f11e391ecda04c7a8efc7bf
Mohd Anis Mohd Mokhtar <mohdanis.mohdmokhtar@mattheyasia.com>
2021-06-16 18:29:44 +0800

    Added new api operation.

--- api/operations.tf
+++ api/operations.tf
@@ -9,3 +9,15 @@ resource "azurerm_api_management_api_operation_policy" "get-users-userId" {
     Backend-Id          = "test-api"
   })
 }
+
+resource "azurerm_api_management_api_operation_policy" "get-users-email" {
+  count               = 1
+  resource_group_name = local.shared-rg 
+  api_management_name = local.apim_name
+  api_name            = azurerm_api_management_api.test-api.name
+  operation_id        = "get-users-email"
+  xml_content = templatefile("api-management/policies/api-operation-basic-auth-policy.xml", {
+    Basic-Auth-UserName = var.basic_auth_username
+    Backend-Id          = "test-api"
+  })
+}
